77a1f1b8b5149b40ebc8bc23ee0dfb080d6991e1
Pishch Daniil <72101790+Nerogen@users.noreply.github.com>
2022-12-13 19:34:38 +0300

    Create lang.cpp

---
+++ lang.cpp
@@ -0,0 +1,14 @@
+gwent_card-> ...
+(* => nrel_gwent_card_description:
+["""
+	RUNAME Детмольд;
+	RUFLVAOUR Такими чарами ....;
+	TYPE UNIT;
+	STRENGTH 6;
+	FACTION fire;
+	ROW RANGED;
+	TYPE NONE;
+	SUBTYPE NONE;
+	ABILITY NONE;
+"""];;
+*);;
